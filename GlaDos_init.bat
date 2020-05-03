1e0765e55ba5881dc7504caf816d5163644543af
aqueleangelo <angelolgoes13@gmail.com>
2020-05-02 21:11:51 -0300

    Added batch file to make bot easier to initiate

---
+++ GlaDos_init.bat
@@ -0,0 +1,6 @@
+@echo off
+start /min "" D:\Programas\VoiceMacro\VoiceMacro.exe
+start "" /B "D:\Programas\WO mic\WOMic\WOMicClient.exe"
+start "" /B "D:\Programas\SoundWire Server\SoundWire Server\SoundWireServer.exe"
+start /min "" C:\Users\angel\Desktop\Projetos\Arduino-GLaDOS\pyScripts\pyServer.py
+@echo on
